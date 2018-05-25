576c4a98fda36042f84c62a75a244511f4f2ffbb
tomwey <tomwey@163.com>
2018-05-25 23:12:10 +0800

    add some func

--- src/pages/redpack-detail/redpack-detail.ts
+++ src/pages/redpack-detail/redpack-detail.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { /*IonicPage, */NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { /*IonicPage, */NavController, NavParams, Platform, Content } from 'ionic-angular';
 import { AppManager } from '../../provider/AppManager';
 import { Redpacks } from '../../provider/Redpacks';
 
@@ -19,23 +19,27 @@ export class RedpackDetailPage {
 
   errorMsg: string = null;
   redpackData: any = null;
+  imgLoaded: boolean = false;
+
+  @ViewChild(Content) content: Content;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private appManager: AppManager,
     private redpacks: Redpacks,
+    private platform: Platform,
   ) {
   }
 
   ionViewDidLoad() {
-    // console.log('ionViewDidLoad RedpackDetailPage');
-    // console.log(this.appManager.shareData);
+    this.fixedIOSScrollBug();
+
     this.redpacks.GetRedback(this.appManager.shareData.rid)
       .then(data => {
         this.errorMsg = null;
 
-        console.log(data['data']);
+        // console.log(data['data']);
 
         if (data && data['data']) {
           this.redpackData = data['data'];
@@ -48,6 +52,29 @@ export class RedpackDetailPage {
         this.errorMsg = error.message;
         // console.log(error);
       });
+  }
+
+  fixedIOSScrollBug() {
+    if (this.platform.is('mobileweb') && this.platform.is('ios')) {
+
+      this.content.scrollTo(0, 1);
+
+      this.content.ionScrollEnd.subscribe(evt => {
+        const scrollElement = this.content.getScrollElement();
+        // console.log(111);
+        if ((this.content.contentHeight + 1) < scrollElement.scrollHeight) {
+
+          if (scrollElement.scrollTop === 0) {
+            // scrollElement.scrollTo(0, 1);
+            this.content.scrollTo(0,1);
+          }
+          else if ((scrollElement.scrollTop + this.content.contentHeight) === scrollElement.scrollHeight) {
+            // scrollElement.scrollTo(0, (scrollElement.scrollTop - 1));
+            this.content.scrollTo(0, (scrollElement.scrollTop - 1));
+          }
+        };
+      });
+    }
   }
 
 }
