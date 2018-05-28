8a789852fbc2ac8f6e673d8d48b0f8a230b5011f
tomwey <tomwey@163.com>
2018-05-28 13:46:34 +0800

    add some func

--- src/pages/redpack-edit/redpack-edit.ts
+++ src/pages/redpack-edit/redpack-edit.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController, App } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { Redpacks } from '../../provider/Redpacks';
 import { Tools } from '../../provider/Tools';
 
