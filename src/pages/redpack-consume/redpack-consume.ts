2eebdba87f1c799ca112cc3bb21304890cac8b9a
tomwey <tomwey@163.com>
2018-05-30 08:41:23 +0800

    add some func

--- src/pages/redpack-consume/redpack-consume.ts
+++ src/pages/redpack-consume/redpack-consume.ts
@@ -19,6 +19,8 @@ export class RedpackConsumePage {
   consume_type: string = 'confirmed';
   consumeData: any = null;
 
+  errorMsg: string = null;
+
   constructor(public navCtrl: NavController,
     private redpacks: Redpacks, 
     public navParams: NavParams) {
@@ -34,16 +36,28 @@ export class RedpackConsumePage {
 
   loadData() {
     this.consumeData = null;
-    
+    this.errorMsg = null;
+
     this.redpacks.GetRedpackConsumes(this.consume_type)
       .then(res => {
         if (res && res['data']) {
           this.consumeData = res['data']
+
+          let list = [];
+          if (this.consumeData && this.consumeData.list) {
+            list = this.consumeData.list;
+          }
+
+          this.errorMsg = list.length == 0 ? '暂无数据' : null;
         }
       })
       .catch(error => {
-
+        this.errorMsg = error.message || error;
       });
+  }
+
+  segmentChanged(ev) {
+    this.loadData();
   }
 
 }
